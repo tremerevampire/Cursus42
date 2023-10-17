9469910fcd08c5fb9a98b9887bdddcbf4e1f6cf0
tremerevampire <139043274+tremerevampire@users.noreply.github.com>
2023-10-17 17:44:38 +0200

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acastejo <acastejo@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 15:58:02 by acastejo          #+#    #+#             */
+/*   Updated: 2023/10/06 13:01:58 by acastejo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*buffer;
+
+	buffer = malloc(count * size);
+	if (buffer != NULL)
+		ft_bzero(buffer, count * size);
+	return (buffer);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acastejo <acastejo@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 16:29:34 by acastejo          #+#    #+#             */
+/*   Updated: 2023/10/15 17:29:16 by acastejo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	l;
+
+	l = ft_strlen(src);
+	i = 0;
+	if (dstsize == 0)
+		return (l);
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	if (l >= dstsize)
+		return (l);
+	return (ft_strlen(dst));
+}
